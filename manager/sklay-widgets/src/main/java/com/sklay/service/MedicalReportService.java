d025ae58d9e0eac5dbaa019b7db26fc08c5c9a3e
1988fuyu@163.com <1988fuyu@163.com>
2014-03-21 17:59:40 +0800

    统计数据报表

--- manager/sklay-widgets/src/main/java/com/sklay/service/MedicalReportService.java
+++ manager/sklay-widgets/src/main/java/com/sklay/service/MedicalReportService.java
@@ -1,5 +1,6 @@
 package com.sklay.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -14,24 +15,25 @@ import com.sklay.model.Group;
 import com.sklay.model.MedicalReport;
 import com.sklay.model.User;
 
-public interface MedicalReportService {
-
-	public Page<MedicalReport> search(String keyword, TipType tipType,
-			ReadType readType, User user, Set<Group> groups, Pageable pageable)
-			throws SklayException;
-
-	public MedicalReport createMedicalReport(MedicalReport report)
-			throws SklayException;
-
-	public long countDayReport(Long userId, long timeStart, long timeEnd,
-			SMSType reportType);
-
-	public List<MedicalReport> getLastReports(Long userId, int size);
-
-	public List<MedicalReport> getMemberReports(MedicalReport lastReport,
-			Long userId, int day);
-
-	public void deleteMedicalReport(Long userId);
-
-	public MedicalReport getMemberLastReports(Long userId);
+public interface MedicalReportService
+{
+    
+    public Page<MedicalReport> search(String keyword, TipType tipType, ReadType readType, User user, Set<Group> groups,
+        Pageable pageable)
+        throws SklayException;
+    
+    public MedicalReport createMedicalReport(MedicalReport report)
+        throws SklayException;
+    
+    public long countDayReport(Long userId, long timeStart, long timeEnd, SMSType reportType);
+    
+    public List<MedicalReport> getLastReports(Long userId, int day);
+    
+    public List<MedicalReport> getLastReports(Long userId, Date dateTime, int day);
+    
+    public List<MedicalReport> getMemberReports(MedicalReport lastReport, Long userId, int day);
+    
+    public void deleteMedicalReport(Long userId);
+    
+    public MedicalReport getMemberLastReports(Long userId);
 }
--- manager/sklay-widgets/src/main/java/com/sklay/service/impl/MedicalReportServiceImpl.java
+++ manager/sklay-widgets/src/main/java/com/sklay/service/impl/MedicalReportServiceImpl.java
@@ -22,87 +22,106 @@ import com.sklay.model.User;
 import com.sklay.service.MedicalReportService;
 
 @Service
-public class MedicalReportServiceImpl implements MedicalReportService {
-
-	@Autowired
-	private MedicalReportDao reportDao;
-
-	@Autowired
-	private SpecificDao specificDao;
-
-	@Override
-	public MedicalReport createMedicalReport(MedicalReport report)
-			throws SklayException {
-		if (null == report)
-			return null;
-		return reportDao.save(report);
-	}
-
-	@Override
-	public long countDayReport(Long userId, long timeStart, long timeEnd,
-			SMSType reportType) {
-		return reportDao.countDayReport(userId, timeStart, timeEnd, reportType);
-	}
-
-	@Override
-	public List<MedicalReport> getLastReports(Long userId, int day) {
-
-		MedicalReport lastReport = specificDao.findLastReport(userId,
-				SMSType.PHYSICAL);
-		if (null == lastReport)
-			return null;
-
-		Long date = lastReport.getReportTime();
-		Date endDate = DateTimeUtil.getDate(date, 1);
-		Date startDate = DateTimeUtil.getDate(date, day);
-
-		Long timeStart = startDate.getTime();
-		Long timeEnd = endDate.getTime();
-
-		return reportDao.getDayReport(userId, timeStart, timeEnd,
-				SMSType.PHYSICAL);
-
-	}
-
-	@Override
-	public MedicalReport getMemberLastReports(Long userId) {
-
-		MedicalReport lastReport = specificDao.findLastReport(userId,
-				SMSType.PHYSICAL);
-		if (null == lastReport)
-			return null;
-
-		return lastReport;
-
-	}
-
-	@Override
-	public List<MedicalReport> getMemberReports(MedicalReport lastReport,
-			Long userId, int day) {
-
-		Long date = lastReport.getReportTime();
-		Date endDate = DateTimeUtil.getDate(date, 1);
-		Date startDate = DateTimeUtil.getDate(date, day);
-
-		Long timeStart = startDate.getTime();
-		Long timeEnd = endDate.getTime();
-
-		return reportDao.getDayReport(userId, timeStart, timeEnd,
-				SMSType.PHYSICAL);
-
-	}
-
-	@Override
-	public void deleteMedicalReport(Long userId) {
-		reportDao.deleteMedicalReport(userId);
-	}
-
-	@Override
-	public Page<MedicalReport> search(String keyword, TipType tipType,
-			ReadType readType, User user, Set<Group> groups, Pageable pageable)
-			throws SklayException {
-
-		return specificDao.searchMedicalReport(keyword, tipType, readType,
-				user, groups, pageable);
-	}
+public class MedicalReportServiceImpl implements MedicalReportService
+{
+    
+    @Autowired
+    private MedicalReportDao reportDao;
+    
+    @Autowired
+    private SpecificDao specificDao;
+    
+    @Override
+    public MedicalReport createMedicalReport(MedicalReport report)
+        throws SklayException
+    {
+        if (null == report)
+            return null;
+        return reportDao.save(report);
+    }
+    
+    @Override
+    public long countDayReport(Long userId, long timeStart, long timeEnd, SMSType reportType)
+    {
+        return reportDao.countDayReport(userId, timeStart, timeEnd, reportType);
+    }
+    
+    @Override
+    public List<MedicalReport> getLastReports(Long userId, int day)
+    {
+        
+        MedicalReport lastReport = specificDao.findLastReport(userId, SMSType.PHYSICAL);
+        if (null == lastReport)
+            return null;
+        
+        Long date = lastReport.getReportTime();
+        Date endDate = DateTimeUtil.getDate(date, 1);
+        Date startDate = DateTimeUtil.getDate(date, day);
+        
+        Long timeStart = startDate.getTime();
+        Long timeEnd = endDate.getTime();
+        
+        return reportDao.getDayReport(userId, timeStart, timeEnd, SMSType.PHYSICAL);
+        
+    }
+    
+    @Override
+    public List<MedicalReport> getLastReports(Long userId, Date dateTime, int day)
+    {
+        
+        if (null == dateTime)
+            return null;
+        
+        Long date = dateTime.getTime();
+        Date endDate = DateTimeUtil.getDate(date, 1);
+        Date startDate = DateTimeUtil.getDate(date, day);
+        
+        Long timeStart = startDate.getTime();
+        Long timeEnd = endDate.getTime();
+        
+        return reportDao.getDayReport(userId, timeStart, timeEnd, SMSType.PHYSICAL);
+        
+    }
+    
+    @Override
+    public MedicalReport getMemberLastReports(Long userId)
+    {
+        
+        MedicalReport lastReport = specificDao.findLastReport(userId, SMSType.PHYSICAL);
+        if (null == lastReport)
+            return null;
+        
+        return lastReport;
+        
+    }
+    
+    @Override
+    public List<MedicalReport> getMemberReports(MedicalReport lastReport, Long userId, int day)
+    {
+        
+        Long date = lastReport.getReportTime();
+        Date endDate = DateTimeUtil.getDate(date, 1);
+        Date startDate = DateTimeUtil.getDate(date, day);
+        
+        Long timeStart = startDate.getTime();
+        Long timeEnd = endDate.getTime();
+        
+        return reportDao.getDayReport(userId, timeStart, timeEnd, SMSType.PHYSICAL);
+        
+    }
+    
+    @Override
+    public void deleteMedicalReport(Long userId)
+    {
+        reportDao.deleteMedicalReport(userId);
+    }
+    
+    @Override
+    public Page<MedicalReport> search(String keyword, TipType tipType, ReadType readType, User user, Set<Group> groups,
+        Pageable pageable)
+        throws SklayException
+    {
+        
+        return specificDao.searchMedicalReport(keyword, tipType, readType, user, groups, pageable);
+    }
 }
